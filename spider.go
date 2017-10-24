895432b08bd2e6fbcd07d3a19862b55fcce5c782
Daniel George Holz <dgholz@gmail.com>
2017-10-24 22:50:59 +0100

    Basic argparsing and requesting

--- spider.go
+++ spider.go
@@ -1,9 +1,33 @@
 package main
 
-import "fmt"
+import ("flag";"fmt";"os";"path")
+import ("net/http";"net/url")
 
-// this is a comment
+func parseArgs() {
+    flag.Usage = func() {
+        fmt.Printf("usage: %s URL\n", path.Base(os.Args[0]))
+        flag.PrintDefaults()
+    }
+
+    flag.Parse()
+    if flag.NArg() == 0 {
+        flag.Usage()
+        os.Exit(1)
+    }
+}
 
 func main() {
-    fmt.Println("Hello World")
+    parseArgs()
+    url, err := url.Parse(flag.Arg(0))
+    if err != nil {
+        fmt.Println("Trouble parsing the site to spider!", err)
+        os.Exit(1)
+    }
+    res, err := http.Get(url.String())
+    if err != nil {
+        fmt.Println("Trouble fetching the URL!", err)
+        os.Exit(1)
+    }
+    res.Body.Close()
+
 }
